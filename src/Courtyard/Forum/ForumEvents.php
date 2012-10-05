c72e6692e004ed5edea9b831cb636cc41f0a838d
Adrian Schneider <siradrian@gmail.com>
2012-10-05 13:34:13 -0700

    added board events

--- src/Courtyard/Forum/ForumEvents.php
+++ src/Courtyard/Forum/ForumEvents.php
@@ -4,14 +4,14 @@ namespace Courtyard\Forum;
 
 final class ForumEvents
 {
-    const POST_CREATE_PRE = 'post.create.pre';
-    const POST_CREATE_POST = 'post.create.post';
+    const BOARD_CREATE_PRE = 'board.create.pre';
+    const BOARD_CREATE_POST = 'board.create.post';
 
-    const POST_UPDATE_PRE = 'post.update.pre';
-    const POST_UPDATE_POST = 'post.update.post';
+    const BOARD_UPDATE_PRE = 'board.update.pre';
+    const BOARD_UPDATE_POST = 'board.update.post';
 
-    const POST_DELETE_PRE = 'post.delete.pre';
-    const POST_DELETE_POST = 'post.delete.post';
+    const BOARD_DELETE_PRE = 'board.delete.pre';
+    const BOARD_DELETE_POST = 'board.delete.post';
 
 
     const TOPIC_CREATE_PRE = 'topic.create.pre';
@@ -22,4 +22,14 @@ final class ForumEvents
 
     const TOPIC_DELETE_PRE = 'topic.delete.pre';
     const TOPIC_DELETE_POST = 'topic.delete.post';
+
+
+    const POST_CREATE_PRE = 'post.create.pre';
+    const POST_CREATE_POST = 'post.create.post';
+
+    const POST_UPDATE_PRE = 'post.update.pre';
+    const POST_UPDATE_POST = 'post.update.post';
+
+    const POST_DELETE_PRE = 'post.delete.pre';
+    const POST_DELETE_POST = 'post.delete.post';
 }
