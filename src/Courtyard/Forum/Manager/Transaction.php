54b7b93611852c07c797c8b8efea71299597d3e6
Adrian Schneider <siradrian@gmail.com>
2012-10-06 10:34:26 -0700

    init arrays

--- src/Courtyard/Forum/Manager/Transaction.php
+++ src/Courtyard/Forum/Manager/Transaction.php
@@ -8,24 +8,30 @@ class Transaction
 {
     protected $firstDispatches;
     protected $secondDispatches;
-    
+
+    public function __construct()
+    {
+        $this->firstDispatches = array();
+        $this->secondDispatches = array();
+    }
+
     public function addFirstPass($eventName, ForumEvent $event)
     {
         $this->firstDispatches[$eventName] = $event;
         return $this;
     }
-    
+
     public function addSecondPass($eventName, ForumEvent $event)
     {
         $this->secondDispatches[$eventName] = $event;
         return $this;
     }
-    
+
     public function getFirstPass()
     {
         return $this->firstDispatches;
     }
-    
+
     public function getSecondPass()
     {
         return $this->secondDispatches;
